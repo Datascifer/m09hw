d2d9b993770b28f2d6e7453f13cd4abe4b034ec7
Datascifer <150779158+Datascifer@users.noreply.github.com>
2024-04-01 00:40:16 -0700

    Update Files

--- demo/setup.py
+++ demo/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='m09hw',
+    name='demo',
     version='0.1',
     description='A simple demonstration package',
     url='http://github.com/ontoligent/m09-demo',
     author='mxg9xv',
     author_email='mxg9xv@virginia.edu',
     license='MIT',
-    packages=['m09hw']
+    packages=['demo']
 )
