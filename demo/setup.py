48f27d2f255277446750407538f333dd9d4706a3
Datascifer <150779158+Datascifer@users.noreply.github.com>
2024-04-01 01:01:33 -0700

    Update Code

--- demo/setup.py
+++ demo/setup.py
@@ -4,7 +4,7 @@ setup(
     name='demo',
     version='0.1',
     description='A simple demonstration package',
-    url='http://github.com/ontoligent/m09-demo',
+    url='https://github.com/Datascifer/m09hw/blob/main/demo',
     author='mxg9xv',
     author_email='mxg9xv@virginia.edu',
     license='MIT',
