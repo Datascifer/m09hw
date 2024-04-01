84b475759675842c96734d77c4016c62b3971d00
Datascifer <150779158+Datascifer@users.noreply.github.com>
2024-03-31 23:31:34 -0700

    Variable Name Change

--- magazinelover_test.py
+++ magazinelover_test.py
@@ -35,7 +35,7 @@ class MagazineLoverTestSuite(unittest.TestCase):
         bl.add_magazine('Moby Dick', 3) 
         bl.add_magazine('War and Peace', 5)
         fav_magazines = bl.fav_magazines()
-        self.assertTrue(all(book['magazine_rating'] > 3 for index, magazine in fav_magazine.iterrows()))  # Checks if all favorite magazines have a rating above 3
+        self.assertTrue(all(magazine['magazine_rating'] > 3 for index, magazine in fav_magazines.iterrows()))  # Checks if all favorite magazines have a rating above 3
         self.assertIn('War and Peace', fav_magazines['magazine_name'].values)
         self.assertNotIn('Moby Dick', fav_magazines['magazine_name'].values)
 
